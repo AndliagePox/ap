05530b6fa8f4d53ee2e5b019154e3b0b2d9ca1b9
Andliage <pox@andliage.com>
2020-01-17 00:19:34 +0800

    leetcode - Solution00002(链表表示的两个数相加)、添加ListNode

---
+++ src/leetcode/ListNode.java
@@ -0,0 +1,18 @@
+/*
+ * Author: Andliage Pox
+ * Date: 2020-01-16
+ */
+
+package leetcode;
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
