97941e33c59e153d2991e42664d4c39cfc86f7fb
Andliage <pox@andliage.com>
2020-01-18 01:04:36 +0800

    leetcode - Solution00094(二叉树的中序遍历)、添加TreeNode

---
+++ src/leetcode/TreeNode.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Andliage Pox
+ * Date: 2020-01-18
+ */
+
+package leetcode;
+
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
