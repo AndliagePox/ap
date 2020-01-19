89a7c464c5d79bad15bad343fa140a9624081454
Andliage <pox@andliage.com>
2020-01-20 00:55:07 +0800

    leetcode - Solution00141(判断环形链表)

---
+++ src/leetcode/Solution00141.java
@@ -0,0 +1,34 @@
+/*
+ * Author: Andliage Pox
+ * Date: 2020-01-20
+ */
+
+package leetcode;
+
+public class Solution00141 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        System.out.println(new Solution00141().hasCycle(head));
+    }
+
+    public boolean hasCycle(ListNode head) {
+        /* 双指针判断环形链表 */
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+        /* 时间O(n)空间O(1) */
+    }
+}
