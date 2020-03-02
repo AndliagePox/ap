1411a2402396741a2c0b19f54ba0f1da03446a7b
Andliage <pox@andliage.com>
2020-03-02 11:06:04 +0800

    leetcode - Solution00189(旋转数组)

---
+++ src/leetcode/Solution00189.java
@@ -0,0 +1,32 @@
+/*
+ * Author: Andliage Pox
+ * Date: 2020-03-02
+ */
+
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution00189 {
+    public static void main(String[] args) {
+        int k = 3;
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        new Solution00189().rotate(nums, k);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reserve(nums, 0, nums.length - 1);
+        reserve(nums, 0, k - 1);
+        reserve(nums, k, nums.length - 1);
+    }
+
+    private void reserve(int[] nums, int start, int end) {
+        for (; start < end; start++, end--) {
+            int t = nums[start];
+            nums[start] = nums[end];
+            nums[end] = t;
+        }
+    }
+}
