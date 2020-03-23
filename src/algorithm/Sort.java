8f7e60d6974b81ebad130fdd8176fba87e19a310
Andliage <pox@andliage.com>
2020-03-24 00:32:55 +0800

    algorithm - 添加冒泡排序、加入随机测试

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -5,65 +5,91 @@
 
 package algorithm;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Sort {
     public static void main(String[] args) {
-        int times = 10000000;
-        int[] a, src = {
-                5, 1, 3, 4, 5, 6, 1, 3, 5, 1, 88, 90, 243, 123,
-                123, 66, 123, 11, 79, 21, 23, 73, 11, 32, 0, 5,
-                123, 22, 212421, 32, 321, 11, 3, 4, 6, 66, 77, 12,
-                32, 41, 21, 43, 667, 2343, 12, 321, 55, 12, 42, 5,
-                1, 32, -12, -42, 12, -22, 41, 80, 22, 21, 667, 21, 2,
-                3, 33, 3, 77, 8, 6, 1, 5, 1, 3, 5, 66, 11322, 5555, 23215,
-                13, 325, 22, 65, 78, 20, 90, -10, 750, 501, 512, 66,
-                44, -22, -30, -91, 51, 60, 2213, 65, 88, 8
-        };
+        int[] a;
+        int arrays = 1000, times = 1000, length = 500;
+        List<int[]> list = new ArrayList<>(arrays);
         long ts, te;
 
+        for (int i = 0; i < arrays; i++) {
+            list.add(generateRandomArray(length));
+        }
+
         ts = System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            a = src.clone();
-            quicksort(a, 0, a.length - 1);
+        for (int[] src: list) {
+            for (int i = 0; i < times; i++) {
+                a = src.clone();
+                quickSort(a, 0, a.length - 1);
+            }
         }
         te = System.currentTimeMillis();
         System.out.println("Self Quick Sort " + (te - ts) + " ms");
 
         ts = System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            a = src.clone();
-            switchSort(a);
+        for (int[] src: list) {
+            for (int i = 0; i < times; i++) {
+                a = src.clone();
+                switchSort(a);
+            }
         }
         te = System.currentTimeMillis();
         System.out.println("Self Switch Sort " + (te - ts) + " ms");
 
         ts = System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            a = src.clone();
-            Arrays.sort(a);
+        for (int[] src: list) {
+            for (int i = 0; i < times; i++) {
+                a = src.clone();
+                Arrays.sort(a);
+            }
         }
         te = System.currentTimeMillis();
         System.out.println("System DPQ Sort " + (te - ts) + " ms");
 
         ts = System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            a = src.clone();
-            selectSort(a);
+        for (int[] src: list) {
+            for (int i = 0; i < times; i++) {
+                a = src.clone();
+                selectSort(a);
+            }
         }
         te = System.currentTimeMillis();
         System.out.println("Self Select Sort " + (te - ts) + " ms");
 
         ts = System.currentTimeMillis();
-        for (int i = 0; i < times; i++) {
-            a = src.clone();
-            doubleSelectSort(a);
+        for (int[] src: list) {
+            for (int i = 0; i < times; i++) {
+                a = src.clone();
+                doubleSelectSort(a);
+            }
         }
         te = System.currentTimeMillis();
         System.out.println("ZYY Double Select Sort " + (te - ts) + "ms");
+
+        ts = System.currentTimeMillis();
+        for (int[] src: list) {
+            for (int i = 0; i < times; i++) {
+                a = src.clone();
+                bubbleSort(a);
+            }
+        }
+        te = System.currentTimeMillis();
+        System.out.println("Self Bubble Sort " + (te - ts) + "ms");
     }
 
-    public static void quicksort(int[] a, int start, int end) {
+    public static int[] generateRandomArray(int size) {
+        int[] arr = new int[(int) (size * Math.random() + 1)];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (10000 * Math.random());
+        }
+        return arr;
+    }
+
+    public static void quickSort(int[] a, int start, int end) {
         if (start >= end) return;
         int x, l = start, r = end;
         x = a[l];
@@ -74,8 +100,8 @@ public class Sort {
             a[r] = a[l];
         }
         a[l] = x;
-        quicksort(a, start, r - 1);
-        quicksort(a, l + 1, end);
+        quickSort(a, start, r - 1);
+        quickSort(a, l + 1, end);
     }
 
     public static void switchSort(int[] a) {
@@ -131,6 +157,18 @@ public class Sort {
             t = a[i];
             a[i] = a[mi];
             a[mi] = t;
+        }
+    }
+
+    public static void bubbleSort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+                }
+            }
         }
     }
 }
