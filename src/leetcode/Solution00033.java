195cd003b2bc6ec167f953cedaff3b65480d65ab
Andliage <pox@andliage.com>
2020-03-24 14:45:53 +0800

    leetcode - Solution00033更改为真二分查找

--- src/leetcode/Solution00033.java
+++ src/leetcode/Solution00033.java
@@ -8,7 +8,7 @@ package leetcode;
 public class Solution00033 {
     public static void main(String[] args) {
         int[] nums = {4, 5, 6, 7, 0, 1, 2};
-        int target = 9;
+        int target = 0;
         System.out.println(new Solution00033().search(nums, target));
     }
 
@@ -37,11 +37,12 @@ public class Solution00033 {
     }
 
     private int findRotatedIndex(int[] nums) {
-        int i;
-        for (i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                return i + 1;
-            }
+        int start = 0, end = nums.length - 1, mid;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (mid + 1 < nums.length && nums[mid] > nums[mid + 1]) return mid + 1;
+            if (nums[mid] < nums[start]) end = mid - 1;
+            else start = mid + 1;
         }
         return 0;
     }
