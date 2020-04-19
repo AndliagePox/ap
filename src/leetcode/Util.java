39ae991699092fc9f97d099c88cdf543ffa7bccc
Andliage <pox@andliage.com>
2020-04-19 16:32:26 +0800

    leetcode - Util添加buildTree

--- src/leetcode/Util.java
+++ src/leetcode/Util.java
@@ -8,6 +8,10 @@ package leetcode;
 import leetcode.ds.ListNode;
 import leetcode.ds.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
 public abstract class Util {
     public static ListNode buildLinkedList(int[] nums) {
         ListNode head = new ListNode(0);
@@ -27,10 +31,43 @@ public abstract class Util {
         System.out.println();
     }
 
+    public static TreeNode buildTree(String s) {
+        s = s.replace(" ", "");
+        String[] nodeStrs = s.split(",");
+        Deque<String> nodes = new ArrayDeque<>(Arrays.asList(nodeStrs));
+        Deque<TreeNode> building = new ArrayDeque<>();
+        if (nodes.isEmpty()) return null;
+
+        String rootStr = nodes.pollFirst();
+        if (rootStr == null || rootStr.equals("null")) return null;
+        TreeNode root = new TreeNode(Integer.valueOf(rootStr));
+
+        building.add(root);
+        while (!building.isEmpty()) {
+            TreeNode curNode = building.pollFirst();
+            if (curNode == null) continue;
+
+            String leftStr = nodes.pollFirst();
+            if (leftStr == null || leftStr.equals("null")) {
+                curNode.left = null;
+            } else {
+                building.add(curNode.left = new TreeNode(Integer.valueOf(leftStr)));
+            }
+
+            String rightStr = nodes.pollFirst();
+            if (rightStr == null || rightStr.equals("null")) {
+                curNode.right = null;
+            } else {
+                building.add(curNode.right = new TreeNode(Integer.valueOf(rightStr)));
+            }
+        }
+        return root;
+    }
+
     public static void printTree(TreeNode root) {
         if (root == null) return;
 
-        System.out.println(root.val + " ");
+        System.out.print(root.val + " ");
         printTree(root.left);
         printTree(root.right);
     }
